d544ae50f7127e724b2c9374fb75ea8d2a82187f
Rabindra Kumar Patra <rabindra@Rabindras-MacBook-Air.local>
2026-09-12 14:10:55 +0530

    handle user context from thread local

---
+++ bookstore-backend/src/main/java/com/bookstore/backend/dtos/UserContext.java
@@ -0,0 +1,10 @@
+package com.bookstore.backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserContext {
+    private String userId;
+}
